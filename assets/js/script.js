31260fb45c3492cb60d69afb7133aa54c01bcbae
Jeana Rose <jr.mathis608@gmail.com>
2020-12-19 16:18:18 -0500

    Deleted psuedo code and unneccessary variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,3 @@
-// DOM MANIPULATION:
-// 1. Create an element
-// 2. Add content
-// 3. Append to an existing element
-
-// DOM Variables (these will be added dynamically)
-// The #city-forecast section
-// var todaysForecast = $("#city-forecast");
-// The #five-day-forecast section
-// Cards will appear dynamically
-// Content inside of the cards will appear dynamically
-
-// What I will need from OpenWeather:
-// - The forecast for the current day depending on which city is selected
-// - The forecast for the next five days depending on which city was selected
-// - Access to the cities through the search function on the app
-
 $(document).ready(function () {
   $("#search-button").on("click", function (event) {
     event.preventDefault();
@@ -25,8 +8,6 @@ $(document).ready(function () {
     var searchedCitiesArray = [];
     var APIkey = "2d55950b982d8809e238650a5988955c";
     var location = $(".form-control").val();
-    var latitudeNum = "";
-    var longitudeNum = "";
     searchedCitiesArray.push(location);
     $.ajax({
       url:
@@ -171,10 +152,6 @@ $(document).ready(function () {
   });
 });
 
-// Event listeners:
-// - When I click the search button, the city I entered is displayed on the page and the
-// city is stored into local storage and displayed in the list-group
-
 // Local storage:
 // The last cities I searched are stored into local storage and displayed in the list-group
 // When an item is stored to local storage:
@@ -182,20 +159,3 @@ $(document).ready(function () {
 // 2. The text is set to the city that was searched
 // 3. The li is appended to to the ul with the class of list-group
 
-// DOM manipulation
-// When a user types a city and clicks the search button:
-// Create an h1
-// Set the text content to have the city, current date, and icon that represents the current weather
-// Append to #city-forecast
-// Create a p
-// Set the text content to be the current temperature
-// Append to #city-forecast
-// Create another p
-// Set the text content to be the current humidity
-// Append to #city-forecast
-// Create another p
-// Set the text content to be the wind speed
-// Append to #city-forecast
-// Create another p
-// Set the text content to be the UV index
-// Append to #city-forecast
